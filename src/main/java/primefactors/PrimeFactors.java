cac65f7cf9e6c1c5434cc4fdd4dd0bb79d2a5669
Jeremy M. Reed <reeje76@gmail.com>
2016-06-29 14:27:28 -0600

    Project setup.
    Import into Eclipse.
    Convert to Maven project.
    Add JUnit 4.12 dependency.
    Move sources to proper packages.
    Add NotImplementedException RuntimeException.
    Stub out PrimeFactors methods.

---
+++ src/main/java/primefactors/PrimeFactors.java
@@ -0,0 +1,12 @@
+package primefactors;
+
+import java.util.List;
+
+import exceptions.NotImplementedException;
+
+public class PrimeFactors
+{
+	public static List<Long> getForNumber(Long input) {
+		throw new NotImplementedException();
+	}
+}
--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -1,3 +1,4 @@
+package primefactors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
